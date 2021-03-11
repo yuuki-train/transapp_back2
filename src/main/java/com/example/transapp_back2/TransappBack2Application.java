922ce5519e16d45c11be73d92f9e9274c40dd50a
yuuki-train <yuuki323-sld51@kcf.biglobe.ne.jp>
2021-03-11 17:14:09 +0900

    transapp_back2 created

---
+++ src/main/java/com/example/transapp_back2/TransappBack2Application.java
@@ -0,0 +1,13 @@
+package com.example.transapp_back2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransappBack2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransappBack2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/transapp_back2/TransappBack2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transapp_back2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransappBack2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
